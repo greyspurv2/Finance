75f3a85a7366fda693edd4ce25b4b9a175423720
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-01-17 20:07:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,48 @@ const express = require("express");
 
 const app = express();
 
-var path = require('path');
-var api = require('./api');
 
+// file retrival for index
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
+
+// file retrival for about
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/about.html");
 });
 
 
+//file retrival for contact
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/contact.html");
 });
 
 
+//file retrival for QA
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/QA.html");
+});
+
+
+//getting the data
+
+app.get('/apidata', (req, res) => {
+    res.json(apidadata)
+});
+
+
+
+// sending the data
+
+app.get('/apidata', (req, res) => {
+    res.send({
+        apidadata: []
+    })
 });
 
 
