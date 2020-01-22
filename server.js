e1034dc6f7e06a9da253f933eeaa29ec7344c7cc
RyanSamman <56226071+RyanSamman@users.noreply.github.com>
2020-01-22 18:37:51 +0300

    Fixed code
    Removed the 20 errors,
    removed extra empty lines
    removed the bare request()s

--- server.js
+++ server.js
@@ -11,115 +11,76 @@ const app = express();
 
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // file retrival for index.html
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 // file retrival for about.html
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/about.html");
 });
 
 
 //file retrival for contact.html
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/contact.html");
 });
 
 
 //file retrival for chart.html
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/chart.html");
 });
 
 
 //file retrival for QA
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/QA.html");
 });
 
 // request with placeholders until API have been chosen 
 
 //real time crypto
-
 //Bitcoin
-
-const APIdataBTC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BTC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BTC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
+const APIdataBTC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BTC&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //Etherum
-
-const APIdataETH = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETHC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETH&apikey=JVQKTJDSYHG974CO", function(error, response, body));
+const APIdataETH = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETHC&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //Litecoin
-const APIdataLTC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BTC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=LTC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
-
+const APIdataLTC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BTC&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //Etherum classic 
-const APIdataETC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETC&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataETC = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=ETC&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //Ripple 
-const APIdataXRP = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=XRP&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=XRP&apikey=JVQKTJDSYHG974CO", function(error, response, body));
+const APIdataXRP = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=XRP&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 
 //time series stocks 
-
 // Apple stock
-
-const APIdataAPPL = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=AAPL&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=AAPL&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataAPPL = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=AAPL&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //google stock 
-
-const APIdataGOOGL = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=GOOGL&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=GOOGL&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataGOOGL = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=GOOGL&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 // S&P500
-const APIdataSP500 = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=SP500&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=SP500&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataSP500 = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=SP500&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 // Beyond meat stock
-const APIdataBYND = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BYND&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BYND&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataBYND = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=BYND&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 //Tesla stock
-
-const APIdataTSLA = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=TSLA&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=TSLA&apikey=JVQKTJDSYHG974CO", function(error, response, body));
-
-
+const APIdataTSLA = request("https://www.alphavantage.co/query?function=TIME_SERIES_5min&symbol=TSLA&apikey=JVQKTJDSYHG974CO", function (error, response, body) { });
 
 
 //specifying the price in a JSON keypair of a stock, crypto or otherwise.
@@ -145,6 +106,6 @@ module.exports = APIdataTSLA;
 
 // listening on port 3000, can be changed depending on the port open upon deployment
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("server is running on port 3000")
 });
